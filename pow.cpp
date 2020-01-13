46229c0ebeefbd2796aa6587e487312f58be5d3f
Divyam Raj <41264059+divyamraj18@users.noreply.github.com>
2020-01-13 11:45:21 +0530

    Update pow.cpp

--- pow.cpp
+++ pow.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int pow(int m,int n)
 {
-	if(n==0)
+	if(n==0)//base case
 	{
 		return 1;
 	}
-	else if(n%2==0)
+	else if(n%2==0)//case when the number is even
 	{
 		return pow(m*m,n/2);
 	}
-	else
+	else//case when the number is odd
 	{
 		return m*pow(m*m,(n-1)/2);
 	}

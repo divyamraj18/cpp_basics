30cfe60b23298b3b12111152393e2d6d46f11e28
divyamraj18 <divyamraj18@gmail.com>
2020-01-13 11:42:49 +0530

    function to find power of a number with less no of multiplication

---
+++ pow.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int pow(int m,int n)
+{
+	if(n==0)
+	{
+		return 1;
+	}
+	else if(n%2==0)
+	{
+		return pow(m*m,n/2);
+	}
+	else
+	{
+		return m*pow(m*m,(n-1)/2);
+	}
+}
+int main()
+{
+	cout<<pow(2,9)<<endl;
+	cout<<pow(2,10)<<endl;
+	return 0;
+}

d5f466529d1731eef3d259c4205f3028e7edf42c
divyamraj18 <divyamraj18@gmail.com>
2020-01-19 01:40:06 +0530

    https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0/

---
+++ geeks_leaders_in_an_array.cpp
@@ -0,0 +1,73 @@
+/*
+https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0/
+Given an array of positive integers. Your task is to find the leaders in the array.
+Note: An element of array is leader if it is greater than or equal to all the elements to its right side. Also, the rightmost element is always a leader. 
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows.
+The first line of each test case contains a single integer N denoting the size of array.
+The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+Print all the leaders.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 107
+0 <= Ai <= 107
+
+Example:
+Input:
+3
+6
+16 17 4 3 5 2
+5
+1 2 3 4 0
+5
+7 4 5 7 3
+Output:
+17 5 2
+4 0
+7 7 3
+
+Explanation:
+Testcase 3: All elements on the right of 7 (at index 0) are smaller than or equal to 7. Also, all the elements of right side of 7 (at index 3) are smaller than 7. And, the last element 3 is itself a leader since no elements are on its right.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	for(int s=0;s<t;s++)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    int i,j;
+	    int b[n];
+	    for(i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    j=0;
+	    int max_right=a[n-1];
+	    b[j++]=max_right;
+	    for(i=n-2;i>=0;i--)
+	    {
+	        if(a[i]>=max_right)
+	        {
+	            max_right=a[i];
+	            b[j++]=a[i];
+	        }
+	        
+	    }
+	    for(i=j-1;i>=0;i--)
+	    {
+	        cout<<b[i]<<" ";
+	    }cout<<endl;
+	    
+	}
+	return 0;
+}
